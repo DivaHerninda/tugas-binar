bc92f562f9a29784620fe50a4924ab2fc7c6e3b3
DivaHerninda <divaherninda0604@gmail.com>
2024-07-20 09:09:55 +0700

    home.dart

---
+++ hotel-booking-api/database/seeders/HotelSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Hotel;
+use Illuminate\Http\Request;
+
+class HotelController extends Controller
+{
+    public function index()
+    {
+        return Hotel::all();
+    }
+}
